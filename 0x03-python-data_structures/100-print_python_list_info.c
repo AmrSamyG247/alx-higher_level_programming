0ab9d610054b922ddc2cc4818b3a0bfad3345b3c
AmrGreens247 <amrsamy111@gmail.com>
2023-06-11 10:43:28 +0000

    Task 14 Calling default c libariaries

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,25 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <Python.h>
-#include <object.h>
-#include <listobject.h>
 
 /**
- * includes listobject.h
- * includes object.h
  * print_python_list_info C function that-
  * prints some basic info about Python lists.
  * @p: is paython list object
 **/
-
-
 void print_python_list_info(PyObject *p)
 {
-	long int size = PyList_Size(p);
 	int i;
-	PyListObject *object = (PyListObject *)p;
 
-	printf("[*] Size of Python List = %li\n", size);
-	printf("[*] Allocated = %li\n", object->allocated);
-	for (i = 0; i < size; i++)
-		printf("Element %i: %s\n", i, Py_TYPE(object->ob_item[i])->tp_name);
+	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < Py_SIZE(p); i++)
+		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
 }
 
