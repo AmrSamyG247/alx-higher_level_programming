97c14e39b2b42a3922a32dacf33711811636daa8
AmrGreens247 <amrsamy111@gmail.com>
2023-06-27 16:34:47 +0000

    Task 10 with Style Python Docstrings

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-"""
-import math class magic class
-"""
+"""Define MagicClass"""
 import math
 
 
 class MagicClass:
-    """
-    class magicclass initialized
-    """
+    """Initialize and define methods area and circumference"""
     def __init__(self, radius=0):
-        self._MagicClass__radius = 0
+        """Initialize MagicClass"""
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-                raise TypeError("radius must be a number")
-        else:
-            self._MagicClass__radius = radius
-        """
-        initializes
-        """
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
     def area(self):
-        return self._MagicClass__radius ** 2 * math.pi
-        """
-        gets area
-        """
+        """Calculate area"""
+        return self.__radius ** 2 * math.pi
+
     def circumference(self):
-        return 2 * math.pi * self._MagicClass__radius
-        """
-        gets circumference
-        """
-if __name__ == "__main__":
-    import dis
-    dis.dis(MagicClass)
+        """Calcualte circumference"""
+        return 2 * math.pi * self.__radius
+
+import dis
+dis.dis(MagicClass)
