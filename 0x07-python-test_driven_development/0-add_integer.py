e27b9d892080aa6767f739c1cf204480e359aca4
AmrGreens247 <amrsamy111@gmail.com>
2023-07-01 11:30:31 +0000

    Python Test driven development Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""Module for add_integer method."""
+"""
+Module 0-add_integer
+Contains one method that returns an int sum
+Accepts two values, whether int or float, and casts them to int before adding
+"""
 
 
 def add_integer(a, b=98):
-    """Adds two integers.
-
-    Args:
-        a: the first integer.
-        b: the second integer, default 98.
-
-    Raises:
-        TypeError: if a, b are not int, float.
-
-    Returns:
-        The sum of the two integers.
     """
-
-    if type(a) not in (int, float):
-        raise TypeError('a must be an integer')
-    if type(b) not in (int, float):
-        raise TypeError('b must be an integer')
-    return int(a) + int(b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/0-add_integer.txt")
+    Returns a + b as int
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
