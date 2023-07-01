2376636f6acc4fe9cf1d8a4438d9a71849a2371a
AmrGreens247 <amrsamy111@gmail.com>
2023-07-01 15:17:14 +0000

    Python Test driven development Task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""
-Module 3-say_my_name
-Contains method that prints out "My name is [full name]"
-Takes in two strings: first and last name
-"""
+"""Module for say_my_name method."""
 
 
 def say_my_name(first_name, last_name=""):
+    """Method for printing first and last name.
+
+    Args:
+        first_name: first name string.
+        last_name: last name string.
+
+    Raises:
+        TypeError: If first_name or last_name are not strings.
     """
-    Prints "My name is [full name]"
-    """
-    if isinstance(first_name, str) and isinstance(last_name, str):
-        print("My name is {:s} {:s}".format(first_name, last_name))
-    else:
-        raise TypeError("{:s} must be a string".
-                        format("first_name" if isinstance(last_name, str)
-                               else "last_name"))
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
