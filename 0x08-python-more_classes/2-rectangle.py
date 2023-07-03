e60ccf39711bc9d7217d7d74a68ef345aae1aeae
AmrGreens247 <amrsamy111@gmail.com>
2023-07-03 11:04:50 +0000

    Task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,7 +3,22 @@
 
 
 class Rectangle:
-    '''This class defines a simple Rectangle.'''
+    """
+    Defines class rectangle with private attribute width and height
+
+    Args:
+        width (int): width
+        height (int): height
+
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+        area(self)
+        perimeter(self)
+    """
 
     def __init__(self, width=0, height=0):
         '''Constructor.
