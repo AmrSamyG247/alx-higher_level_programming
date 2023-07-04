f3f8399b4ccbf3f7aab9bf22d4156c1bbaf2a657
AmrGreens247 <amrsamy111@gmail.com>
2023-07-04 14:27:10 +0000

    Task 30 pycodestyle

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-'''Module for LockedClass.'''
-
 
 class LockedClass:
-    '''A class demonstrating locked slots prevents creating attributes.'''
-    __slots__ = 'first_name',
-
+    __slots__ = ['first_name']
